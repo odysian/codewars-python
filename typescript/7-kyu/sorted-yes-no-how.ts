cb1813461d115bde2830d5339cf122c60594dd07
odysian <odyssian7@gmail.com>
2026-01-06 15:58:34 -0500

    add 7kyu sorted yes no how

---
+++ typescript/7-kyu/sorted-yes-no-how.ts
@@ -0,0 +1,49 @@
+// https://www.codewars.com/kata/580a4734d6df748060000045/train/typescript
+
+
+// Initial attempt: Counter pattern
+export function isSortedAndHow(array: number[]): string {
+  let asc_count: number = 1
+  let desc_count: number = 1
+
+  for (let i = 0; i < array.length - 1; i++) {
+
+    if (array[i] <= array[i+1]){
+      asc_count++
+    } 
+    if (array[i] >= array[i+1]) {
+      desc_count++
+    }
+  }
+
+  if (asc_count === array.length) {
+    return "yes, ascending"
+  } else if (desc_count === array.length) {
+    return "yes, descending"
+  } else {
+    return "no"
+  }
+}
+
+// Smarter approach: Flag pattern 
+export function isSortedAndHow1(array: number[]): string {
+  let ascending: boolean = true;
+  let descending: boolean = true;
+
+  for (let i = 0; i < array.length - 1; i++) {
+
+    if (array[i] > array[i+1]){
+      ascending = false;
+    } 
+    if (array[i] < array[i+1]) {
+      descending = false;
+    }
+    if (!ascending && !descending) {
+      return "no";
+    }
+  }
+
+  if (ascending) return "yes, ascending";
+  if (descending) return "yes, descending";
+  return "no";
+}
